5e0a8fb175b88d174144dde588123c49f28b6c2d
Sujeet Shetty <sujeet.shetty@inscripts.com>
2018-07-20 19:53:39 +0530

    Adding New Sample App

---
+++ CometChatSample/MyFirebaseIIDService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+
+namespace CometChatSample
+{
+	[Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService Firebase";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+			System.Console.WriteLine(TAG+ "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+        void SendRegistrationToServer(string token)
+        {
+			System.Console.WriteLine(TAG + "SendRegistrationToServer token: " + token);
+            // Add custom implementation, as needed.
+        }
+    }
+}
---
+++ CometChatSample/MyFirebaseMessagingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Util;
+using Firebase.Messaging;
+using Utils;
+
+namespace CometChatSample
+{
+	[Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService Firebase";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+			CCNotificationHelper.ProcessCCNotificationData(this,message,Resource.Drawable.cometchat_logo,Resource.Drawable.cometchat_logo);
+        }
+    }
+}
